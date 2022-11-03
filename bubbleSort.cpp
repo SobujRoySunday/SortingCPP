cfe606d00c5f83c5051efc6f05e4a71b480c153d
Sorbopriyo Roy <78290444+SobujRoySunday@users.noreply.github.com>
2022-11-03 18:36:15 +0530

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,34 +1,31 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-void bubble_sort(int arr[], int n) {
-  // bubble sort
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+
+void bubbleSort(int arr[], int n){
+    // Sorting the array
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; i < n - j - 1; j++){
+            if(arr[j] > arr[j + 1]) swap(arr[j], arr[j + 1]);
+        }
     }
-  }
-
-  cout << "After Using bubble sort: " << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
 }
-int main() {
-    int arr[] = {13,46,24,52,20,9};
-    cout<<"Before Using Bubble Sort: "<<endl;
-    for(int i=0;i<6;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubble_sort(arr, n);
+
+int main(){
+    // Getting size of the array
+    int n;
+    cout<<"Size of the array: ";
+    cin>>n;
+    int arr[n], arrCopy[n];
+    
+    // Storing elements of array
+    cout<<"Enter Values:"<<endl;
+    for(int i = 0; i < n; i++) cin>>arr[i];
+
+    bubbleSort(arr, n);
+
+    // Print Array
+    cout<<"Sorted Array:\n";
+    for(int i = 0; i < n; i++) cout<<arr[i]<<" ";
+
     return 0;
 }
