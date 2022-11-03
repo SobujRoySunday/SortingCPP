844b69ce0a3b223bb6ca485af5765d06c78135df
Sorbopriyo Roy <78290444+SobujRoySunday@users.noreply.github.com>
2022-11-03 19:18:18 +0530

    Update selectionSort.cpp

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,35 +1,33 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-void selection_sort(int arr[], int n) {
-  // selection sort
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
-        mini = j;
-      }
+
+void selectionSort(int arr[], int n){
+    // Sorting the array
+    for(int i = 0; i < n - 1; i++){
+        int mini = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[mini]) mini = j;
+        }
+        swap(arr[i], arr[mini]);
     }
-    int temp = arr[mini];
-    arr[mini] = arr[i];
-    arr[i] = temp;
-  }
-
-  cout << "After selection sort: " << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
 }
-int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-   cout << "Before selection sort: " << "\n";
-   for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-  selection_sort(arr, n);
-  return 0;
+
+int main(){
+    // Getting size of the array
+    int n;
+    cout<<"Size of the array: ";
+    cin>>n;
+    int arr[n], arrCopy[n];
+    
+    // Storing elements of array
+    cout<<"Enter Values:"<<endl;
+    for(int i = 0; i < n; i++) cin>>arr[i];
+
+    selectionSort(arr, n);
+
+    // Print Array
+    cout<<"Sorted Array:\n";
+    for(int i = 0; i < n; i++) cout<<arr[i]<<" ";
+
+    return 0;
 }
