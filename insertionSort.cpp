5911d6cedc4a07653c16232af25b17dfe7219d40
Sorbopriyo Roy <78290444+SobujRoySunday@users.noreply.github.com>
2022-11-03 19:51:43 +0530

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,33 +1,34 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
-void insertion_sort(int arr[], int n) {
-  // insertion sort
-  for (int i = 1; i < n; i++) {
-    int current = arr[i];
-    int j = i - 1;
-    while (j >= 0 && arr[j] > current) {
-      //swap
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = current;
-  }
-  cout << "After insertion sort:" << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
 
+void insertionSort(int arr[], int n){
+    for(int i = 1; i < n; i++){
+        int current = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > current){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
 }
-int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  cout << "Before insertion sort:" << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-  insertion_sort(arr, n);
-  return 0;
+
+int main(){
+    // Getting size of the array
+    int n;
+    cout<<"Size of the array: ";
+    cin>>n;
+    int arr[n], arrCopy[n];
+    
+    // Storing elements of array
+    cout<<"Enter Values:"<<endl;
+    for(int i = 0; i < n; i++) cin>>arr[i];
+
+    insertionSort(arr, n);
+
+    // Print Array
+    cout<<"Sorted Array:\n";
+    for(int i = 0; i < n; i++) cout<<arr[i]<<" ";
+
+    return 0;
 }
